99d078f69fd5a7a8ad39cd51f82e9d3365733dfc
Caden Lalley <cadenlalley@gmail.com>
2023-04-25 23:22:24 -0400

    Fix libraries, again

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pyttsx3
 
-# Initialize Text-to-Speech Engine
 engine = pyttsx3.init()
 voices = engine.getProperty("voices")
-engine.setProperty("voices", voices[1].id)
+engine.setProperty("voice", voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+speak("nipples")
